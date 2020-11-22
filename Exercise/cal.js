89fa5e828ca7f011ed3b27a652ecc8c4fd54bdab
helloworld-jisoo <helloworld.jisoo@gmail.com>
2020-11-22 21:51:30 +0000

    exercise update.

--- Exercise/cal.js
+++ Exercise/cal.js
@@ -1,126 +1,25 @@
+// how to get values from HTML? LIKE, WHEN THE SIZE OR NUMBER IS CHANGED, I WANNA GET DATA FROM HTML?
+const size = document.querySelector('select'); 
+const result = document.querySelector('.result');
+const number = document.querySelector('input');
 
-/* Try 5
-const quantity = document.getElementById('quantity');
-const size = document.getElementById('size');
-const total = 0;
+size.onchange = handleChange;
+number.onchange = handleChange;
 
-function calculation(){
-  if(size.value=="None"){
-		alert("Please select a milk");
-		return;
-	}else if(size.value=="Large"){
-		total = quantity * 5.00;
-	}else if(size.value=="Extra-Large"){
-    total = quantity * 7.00;
-	}else if(size.value =="Super-Size"){
-    total = quantity * 17.00;
-  }
-  alert('The coffee is worth €'+parseFloat(total).toFixed(2));
-}
-/*
-
-/* Try 4
-const select = document.querySelector('select');
-const number = document.querySelector('.result');
-
-select.addEventListener('change',  sizeValue);
-function sizeValue() {
-  const choice = select.value;
-  if (choice === 'Large') {
-    sizeValue = 5.00;
-  } else if (choice === 'Extra-large') {
-    sizeValue = 7.00;
-  } else if (choice === 'Super-Size') {
-    sizeValue =17.00;} 
-    else (choice === 'none')
-  sizeValue = 0.00;
-}*/
-
-
-
-/* Try 3
-const selectElement = document.querySelector('#size');
-const sizeValue = new Array();
-size["None"]=0;
-size["Large"]=5.00;
-size["Extra-large"]=7.00;
-size["Super-Size"]=17.00;
-
-selectElement.addEventListener('change', (event) => {
-  const result = document.querySelector('.result');
-  result.textContent = `Your Pizza size is ${event.target.value}`;
-});
-
-const input = document.querySelector('#quantity');
-const log = document.getElementById('log');
-
-input.addEventListener('change', updateValue);
-
-function updateValue(e) {
-  log.textContent = e.target.value;
+function handleChange(e) { // how to use handleChange?
+  parseInt.
+  const result = event(size,number);
+  console.log(result);
 }
 
-total = sizeValue * updateValue;
-console.log(total); */
 
-/* Try 2
-const number = document.getElementById('quantity').value;
-
-
-function getSizaPizzaPrice(){
-  let pizzaSizaPrice = 0;
-  let form = document.forms["form"];
-  let selectedPizzaSize = form.elements["size"];;
-  pizzaSizePrice = size[selectedPizzaSize.value];
-  return pizzaSizaPrice;
+function multiply(size, number){
+  return size * number;
 }
 
-function calculateTotal() {
-    let pizzaPrice = getSizaPizzaPrice() * number; // not working?
-    let total = document.getElementById('totalPrice');
-    total.style.display='block';
-    total.innerHTML = "Total Price For the  €"+  pizzaPrice;
-}
-*/
-
-/*let number = document.getElementById('quantity').value;
-let size = document.getElementById('size').value;
-
-let Size = new Array();
-      pizza_price["None"]=0;
-      pizza_price["Large"]=5;
-      pizza_price["Extra-large"]=7;
-      pizza_price["Super-Size"]=17;
-
-      total = (number * size)
-      document.getElementById('total-price').style.innerHTML = total;
-      
-btn.addEventListener('click', function(){
- Try 1 */
-const Size = new Array();
- Size["None"]=0;
- Size["Large"]=5;
- Size["Extra-large"]=7;
- Size["Super-Size"]=17;
-
-function getPizzaNumberPrice(){
-  let pizzaNumberPrice = 0;
-  const quantity = document.getElementById('quantity');
-  const selectedNumber = quantity.getElementById
-  pizzaNumberPrice = quantity[selectedNumber.value];
-  return pizzaNumberPrice;
-
-}
-function getPizzaSizePrice(){
-let pizzaSizePrice=0;
-pizzaSizePrice = pizza_price[selectedPizza.value];
-  return pizzaSizePrice;
+function print(size, number) {
+  console.log(`${size} ${number}`);
 }
 
-function calculateTotal() {
-  const pizzaPrice = getPizzaSizePrice() * getPizzaNumberPrice();
+handleChange(multiply);
 
-  let total = document.getElementById('total_value');
-  total.style.display='block';
-  toaal.innerHTML = "TOTAL PRICE FOR THE PIZZA €"+pizzaPrice;
-}
